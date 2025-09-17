65a4d16a36acd8d9b4b39d282d6bbc270c80fa7f
nolandex <stemapal1234@gmail.com>
2025-09-18 02:41:53 +0700

    Update hero-section.tsx

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -3,7 +3,7 @@ export function HeroSection() {
     <section className="relative w-full px-4 py-2 mt-0">
       <div className="relative w-full h-[180px] md:h-[250px] lg:h-[300px] overflow-hidden rounded-xl shadow-lg">
         <img
-          src="https://ik.imagekit.io/nf7nyedso/1000376923.png?updatedAt=1758076777922"
+          src="https://ik.imagekit.io/nf7nyedso/Blue%20Marketing%20Basic%20Course%20Banner_20250918_023808_0000.jpg?updatedAt=1758138006252"
           alt="Hero Banner"
           className="absolute inset-0 w-full h-full object-cover"
         />
